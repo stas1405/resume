ac9a7265047b914d025056e326ec748cd322bd24
Stassilla <stas1405@yandex.ru>
2015-10-07 12:05:03 +0300

    first resume updated after review

--- js/helper.js
+++ js/helper.js
@@ -126,7 +126,7 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(contacts.location);
 
     // iterates through school locations and appends each location to
     // the locations array
